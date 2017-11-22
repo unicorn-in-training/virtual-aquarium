80e6b4106848e86c0ac91cc74ad0c0441e5b7b7e
Susie <susaluda@gmail.com>
2017-11-22 11:56:02 -0800

    Adjust height or speed of fish
    Added functionality where the user can use the up/down arrow keys to adjust the height on the page of the fish's sinusoidal path within certain bounds. The user can also use the left and right arrows to slow down and speed up the fish, respectively. But as of this version, the change in speed will only take place once the fish changes direction and therefore is not immediately visible.

--- aquariumscript.js
+++ aquariumscript.js
@@ -1,14 +1,34 @@
+/*jshint esversion:6, laxbreak:true, expr:true*/
 window.onload = function () {
     var angelfishR = new Image(),
-    clownfishR = new Image(),
-    goofyfishR = new Image(),
-    happyfishR = new Image(),
-    angelfishL = new Image(),
-    clownfishL = new Image(),
-    goofyfishL = new Image(),
-    happyfishL = new Image();
+        clownfishR = new Image(),
+        goofyfishR = new Image(),
+        happyfishR = new Image(),
+        angelfishL = new Image(),
+        clownfishL = new Image(),
+        goofyfishL = new Image(),
+        happyfishL = new Image();
 
     var debug = false;
+    var addSpeed = 0;
+    var addHeight = 0;
+    document.addEventListener('keydown', function (event) {
+        const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+        switch (key) {
+        case 'ArrowRight':
+            addSpeed += 5;
+            break;
+        case 'ArrowLeft':
+            addSpeed += -5;
+            break;
+        case 'ArrowUp':
+            addHeight += 5;
+            break;
+        case 'ArrowDown':
+            addHeight += -5;
+            break;
+        }
+    });
 
     var randomNumber = function () {
         var number = Math.round(Math.random() * 70);
@@ -20,7 +40,6 @@ window.onload = function () {
 
     var swimmingFish = function (direction, imageName) {
         direction = direction || "R";
-        console.log(imageName);
         var elem = document.getElementById("animate");
         var debugText = document.getElementById("debug");
         var debugOutput = "";
@@ -30,42 +49,58 @@ window.onload = function () {
         var w = window.innerWidth;
         var h = window.innerHeight;
         var pos = 0;
-        interval = setInterval(frame, 40);
+        var intervalLength = 40 - addSpeed;
+        if (intervalLength <= 1) {
+            intervalLength = 1;
+            console.log("Can't go any faster! " + intervalLength);
+        } else if (intervalLength >= 200) {
+            intervalLength = 200;
+            console.log("Too slow! " + intervalLength);
+        }
+        interval = setInterval(frame, intervalLength);
         var sineCalc = 0;
         var x = 0;
         var y = 0;
-        var offset = h - 0.7 * h;
+
         var switchDirection = 0;
         var amplitude = randomNumber();
-        var imgWidth = '';
-        var imgHeight = '';
-        var img=new Image();
+        var imgWidth = 0;
+        var imgHeight = 0;
+        var img = new Image();
         if (direction == "R") {
             img.src = 'images/' + imageName + '-r.png';
-            console.log(elem.innerHTML = '<img src="images/' + imageName + '-r.png" id="' + imageName+'">');
+            elem.innerHTML = '<img src="images/' + imageName + '-r.png" id="' + imageName + '">';
             imgWidth = img.width;
             imgHeight = img.height;
 
         } else if (direction == "L") {
             img.src = 'images/' + imageName + '-l.png';
             x = w - imgWidth;
-            elem.innerHTML = '<img src="images/' + imageName + '-l.png" id="' + imageName+'">';
+            elem.innerHTML = '<img src="images/' + imageName + '-l.png" id="' + imageName + '">';
             imgWidth = img.width;
             imgHeight = img.height;
         }
-        if (imgWidth==0){
+        if (imgWidth == 0) {
             imgWidth = 200;
         }
-        if (imgHeight==0){
+        if (imgHeight == 0) {
             imgHeight = 200;
         }
-        if(debug){
+        if (debug) {
             console.log(imgWidth);
             console.log(imgHeight);
         }
 
-
         function frame() {
+            var offset = Math.round(h - 0.7 * h) - addHeight;
+            if (offset >= h - 200) {
+                offset = h - 200;
+                console.log("Too low on page " + offset);
+            } else if (offset <= 30) {
+                offset = 30;
+                console.log("Too high on page " + offset);
+            }
+            console.log(offset);
             if (y > offset - 3 && y < offset + 3) {
                 amplitude = randomNumber();
             }
@@ -88,7 +123,7 @@ window.onload = function () {
                     newDirection = "R";
                 }
                 iterationCounter++;
-                swimmingFish(newDirection,imageName);
+                swimmingFish(newDirection, imageName);
             } else {
                 pos++;
                 sineCalc = Math.sin(pos * 0.08);
@@ -114,32 +149,31 @@ window.onload = function () {
         clownfishL.src = 'images/clown-fish-l.png';
         goofyfishL.src = 'images/goofy-fish-l.png';
         happyfishL.src = 'images/happy-fish-l.png';
-        console.log('preload done');
-        happyfishL.onload = swimmingFish('R','happy-fish');
+        happyfishL.onload = swimmingFish('R', 'happy-fish');
     }
     preload();
 
-    $(".btn-fish").on('click',function() {
+    $(".btn-fish").on('click', function () {
         var fishtype = $(this).text();
         var imageName = '';
-        switch(fishtype){
-            case 'Happy Fish':
-                imageName = 'happy-fish';
-                break;
-            case 'Goofy Fish':
-                imageName = 'goofy-fish';
-                break;
-            case 'Angel Fish':
-                imageName = 'angel-fish';
-                break;
-            case 'Clown Fish':
-                imageName = 'clown-fish';
-                break;
-            default:
-                imageName = 'happy-fish';
-                break;
+        switch (fishtype) {
+        case 'Happy Fish':
+            imageName = 'happy-fish';
+            break;
+        case 'Goofy Fish':
+            imageName = 'goofy-fish';
+            break;
+        case 'Angel Fish':
+            imageName = 'angel-fish';
+            break;
+        case 'Clown Fish':
+            imageName = 'clown-fish';
+            break;
+        default:
+            imageName = 'happy-fish';
+            break;
         }
         clearInterval(interval);
-        swimmingFish('R',imageName);
+        swimmingFish('R', imageName);
     });
 };
